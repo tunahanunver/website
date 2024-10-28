7b0d3fb2e398d3fccc02f993c64670f160dbb791
tunahanunvr <tunahanunver26@gmail.com>
2024-10-28 22:21:22 +0300

    scroll deneme

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const navToggle = document.querySelector(".nav_toggle"),
       navMenu = document.querySelector(".nav_menu"),
-      navLinks = document.querySelectorAll(".nav_link"); // Menüdeki linkleri seçiyoruz
+      navLinks = document.querySelectorAll(".nav_link");
 
 // Menü toggle işlevi
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("show_menu");
     navToggle.classList.toggle("active");
+    document.body.classList.toggle("no_scroll"); // Scroll engelleme/toggle
 });
 
 // Her bir linke tıklanıldığında menüyü kapatma işlevi
@@ -13,9 +14,9 @@ navLinks.forEach(link => {
     link.addEventListener("click", () => {
         navMenu.classList.remove("show_menu");
         navToggle.classList.remove("active");
+        document.body.classList.remove("no_scroll"); // Scroll'u geri getir
     });
 });
-
 // TYPİNG TEXT
 
 var typed = new Typed(".profession_text", {
